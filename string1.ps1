a95b73bf0520073e8ec1e2ff6b7d7ce8fe195b2d
Danij123 <ddanijj@gmail.com>
2017-10-25 21:44:21 +0300

    Create string1.ps1

---
+++ string1.ps1
@@ -0,0 +1,1 @@
+$wifi_ru=(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Содержимое ключа\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize 
